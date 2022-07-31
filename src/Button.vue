a3883667854b12bcd74e563907b1e9b12e9afeb0
BarryDong <286084845@qq.com>
2022-07-31 15:17:06 +0800

    使用 parcel 打包 & 将 Button.js 重构为 Button.vue

---
+++ src/Button.vue
@@ -0,0 +1,23 @@
+<template>
+  <button class="g-button">按钮</button>
+</template>
+
+<script>
+export default {
+  name: 'Button'
+}
+</script>
+
+<style scoped lang="scss">
+.g-button {
+  height: var(--button-height);
+  background: var(--button-bg);
+  font-size: var(--font-size);
+  border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);
+  padding: 0 1em;
+  &:hover { border-color: var(--border-color-hover); }
+  &:active { background: var(--button-bg-active); }
+  &:focus { outline: none; }
+}
+</style>
