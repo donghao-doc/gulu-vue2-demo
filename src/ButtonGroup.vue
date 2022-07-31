f430d59e6367043aba3c9138194caa4152619fb8
BarryDong <286084845@qq.com>
2022-07-31 21:50:53 +0800

    创建 ButtonGroup.vue

---
+++ src/ButtonGroup.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="g-button-group">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ButtonGroup'
+}
+</script>
+
+<style scoped lang="scss">
+.g-button-group {
+  display: inline-flex;
+  vertical-align: middle;
+  > .g-button {
+    border-radius: 0;
+    &:first-child { border-radius: var(--border-radius) 0 0 var(--border-radius); }
+    &:last-child { border-radius: 0 var(--border-radius) var(--border-radius) 0; }
+    &:not(:first-child) { margin-left: -1px; }
+    &:hover { position: relative; z-index: 1; }
+  }
+}
+</style>
--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 
 import Button from './Button.vue'
 import Icon from './Icon.vue'
+import ButtonGroup from './ButtonGroup.vue'
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
+Vue.component('g-button-group', ButtonGroup)
 
 new Vue({
   el: '#app',
