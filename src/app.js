299994b56d4c5d9b655e6a37c0a25b4910acfc09
BarryDong <286084845@qq.com>
2022-08-06 11:03:14 +0800

    使用 chai.expect 测试

--- src/app.js
+++ src/app.js
@@ -8,6 +8,8 @@ Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
 Vue.component('g-button-group', ButtonGroup)
 
+Vue.config.productionTip = false
+
 new Vue({
   el: '#app',
   data: {
@@ -16,3 +18,99 @@ new Vue({
     loading3: false
   }
 })
+
+/**
+ * 单元测试
+ */
+import chai from 'chai'
+const { expect } = chai
+
+{
+  // Button 组件 - 只传一个 icon
+  const Constructor = Vue.extend(Button)
+  const vc = new Constructor({
+    propsData: {
+      icon: 'settings'
+    },
+  })
+  // vc.$mount('#test')   // 挂载到元素上
+  vc.$mount()             // 也可以挂载到内存里
+  const useElement = vc.$el.querySelector('use')
+  const href = useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#i-settings')
+  vc.$el.remove()
+  vc.$destroy()
+}
+
+{
+  // Button 组件 - 传 icon, loading
+  const Constructor = Vue.extend(Button)
+  const vc = new Constructor({
+    propsData: {
+      icon: 'settings',
+      loading: true
+    },
+  })
+  vc.$mount()
+  const useElement = vc.$el.querySelector('use')
+  const href = useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#i-loading')
+  vc.$el.remove()
+  vc.$destroy()
+}
+
+{
+  // Button 组件 - order
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  const vc = new Constructor({
+    propsData: {
+      icon: 'settings'
+    },
+  })
+  // button.$mount()   // 如果元素不挂载到页面上，页面就不渲染这个元素，css 设置的样式也就不会加到这个元素上，所以下方获取的 order 就为空
+  vc.$mount(div)
+  const svgElement = vc.$el.querySelector('svg')
+  const { order } = window.getComputedStyle(svgElement)
+  expect(order).to.eq('1')
+  vc.$el.remove()
+  vc.$destroy()
+}
+
+{
+  // Button 组件 - order - 传 icon-position
+  const div1 = document.createElement('div')
+  document.body.appendChild(div1)
+  const Constructor = Vue.extend(Button)
+  const vc = new Constructor({
+    propsData: {
+      icon: 'settings',
+      iconPosition: 'right'
+    },
+  })
+  vc.$mount(div1)
+  const svgElement = vc.$el.querySelector('svg')
+  const { order } = window.getComputedStyle(svgElement)
+  expect(order).to.eq('2')
+  vc.$el.remove()
+  vc.$destroy()
+}
+
+// {
+//   // Button 组件 - click 事件
+//   const Constructor = Vue.extend(Button)
+//   const vc = new Constructor({
+//     propsData: {
+//       icon: 'settings',
+//     },
+//   })
+//   vc.$mount()
+//   vc.$on('click', function () {
+//     console.log(1)
+//   })
+//   const button = vc.$el
+//   button.click()
+//   vc.$el.remove()
+//   vc.$destroy()
+// }
