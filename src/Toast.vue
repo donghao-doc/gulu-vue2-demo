f5a33fc5545111d6ca079510789b1020613b6cc9
BarryDong <286084845@qq.com>
2022-08-21 09:42:49 +0800

    Toast 组件-基本样式

--- src/Toast.vue
+++ src/Toast.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>Toast</div>
+  <div class="g-toast">
+    <slot></slot>
+  </div>
 </template>
 
 <script>
@@ -9,5 +11,15 @@ export default {
 </script>
 
 <style scoped lang="scss">
-
+$font-size: 14px;
+$toast-height: 40px;
+$toast-bg: rgba(0, 0, 0, 0.75);
+.g-toast {
+  color: #fff; font-size: $font-size;
+  height: $toast-height; line-height: 1.8;
+  background: $toast-bg; border-radius: 4px; box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.5);
+  padding: 0 1em;
+  display: flex; align-items: center;
+  position: fixed; left: 50%; top: 0; transform: translateX(-50%);
+}
 </style>
