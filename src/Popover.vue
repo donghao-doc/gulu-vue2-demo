839e9a7fe5a4a30cc6c77dafe100b57ba8307f55
BarryDong <286084845@qq.com>
2022-09-04 12:25:35 +0800

    Popover 组件-将 content-wrapper 放到 body 中

--- src/Popover.vue
+++ src/Popover.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="g-popover" @click.stop="handleTrigger">
-    <div v-if="visible" class="content-wrapper" @click.stop>
+    <div v-if="visible" ref="contentWrapper" class="g-popover-content-wrapper" @click.stop>
       <slot name="content"></slot>
     </div>
-    <slot></slot>
+    <div ref="triggerWrapper" class="trigger-wrapper">
+      <slot></slot>
+    </div>
   </div>
 </template>
 
@@ -24,6 +26,13 @@ export default {
           document.removeEventListener('click', eventHandler)
         }
         document.addEventListener('click', eventHandler)
+        this.$nextTick(() => {
+          const { contentWrapper, triggerWrapper } = this.$refs
+          const { left, top } = triggerWrapper.getBoundingClientRect()
+          contentWrapper.style.left = left + window.scrollX + 'px'
+          contentWrapper.style.top = top + window.scrollY + 'px'
+          document.body.appendChild(this.$refs.contentWrapper)
+        })
       }
     }
   }
@@ -31,17 +40,21 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.g-popover-content-wrapper {
+  border: 1px solid green;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+  position: absolute;
+  left: 0;
+  top: 0;
+  transform: translateY(-100%);
+}
+
 .g-popover {
-  border: 1px solid red;
   display: inline-block;
   vertical-align: middle;
   position: relative;
-  > .content-wrapper {
-    border: 1px solid green;
-    box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
-    position: absolute;
-    left: 0;
-    bottom: 100%;
+  > .trigger-wrapper {
+    border: 1px solid red;
   }
 }
 </style>
