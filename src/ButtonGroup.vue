d2478656ba48d7f0eb27d8d7b1d273913bdbeb53
BarryDong <286084845@qq.com>
2022-09-11 14:49:48 +0800

    重构 ButtonGroup.vue CSS 代码

--- src/ButtonGroup.vue
+++ src/ButtonGroup.vue
@@ -19,13 +19,15 @@ export default {
 </script>
 
 <style scoped lang="scss">
+$border-radius: 4px;
+
 .g-button-group {
   display: inline-flex;
   vertical-align: middle;
   > .g-button {
     border-radius: 0;
-    &:first-child { border-radius: var(--border-radius) 0 0 var(--border-radius); }
-    &:last-child { border-radius: 0 var(--border-radius) var(--border-radius) 0; }
+    &:first-child { border-radius: $border-radius 0 0 $border-radius; }
+    &:last-child { border-radius: 0 $border-radius $border-radius 0; }
     &:not(:first-child) { margin-left: -1px; }
     &:hover { position: relative; z-index: 1; }
   }
